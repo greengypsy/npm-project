fb51ef7aed1928fb1e41afb445e06cacf06cdd3a
greengypsy <ckumar073@gmail.com>
2022-12-22 18:15:13 +1030

    added handlebars files and code to index.js

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ https://levelup.gitconnected.com/set-up-and-run-a-simple-node-server-project-38b
 
 https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories
 
+https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-nodejs#using-the-nodejs-starter-workflow
+
 
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
 
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils" }));
+
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
